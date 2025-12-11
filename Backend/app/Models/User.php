b0bd8aa520601adbeb125627f6f4174c142c320c
Ianloking <gtzian16@gmai.com>
2025-12-11 16:32:21 -0600

    Backend actualizado1

--- Backend/app/Models/User.php
+++ Backend/app/Models/User.php
@@ -2,9 +2,23 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Notifications\Notifiable;
 
-class User extends Model
+class User extends Authenticatable
 {
-    //
+    use HasApiTokens, Notifiable;
+
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+        'role',
+    ];
+
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
 }
